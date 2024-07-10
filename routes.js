a5f0f3201f780d27177b1b5a6af086e60c9d74ff
Mahdi Ihsan <mahdiihsan16110@gmail.com>
2024-07-10 17:06:51 +0600

    Data Source, could be replaced with a real database

--- routes.js
+++ routes.js
@@ -7,6 +7,39 @@ const todos = [
     { id: '6', title: 'Sixth Todo' },
 ];
 
+// // Data Source, could be replaced with a real database
+// const todos = [
+//     {
+//       title: "Todo 1",
+//       desc: "This is my first Todo",
+//       completed: true,
+//     },
+  
+//     {
+//       title: "Todo 2",
+//       desc: "This is my second Todo",
+//       completed: true,
+//     },
+  
+//     {
+//       title: "Todo 3",
+//       desc: "This is my third Todo",
+//       completed: true,
+//     },
+  
+//     {
+//       title: "Todo 4",
+//       desc: "This is my fourth Todo",
+//       completed: true,
+//     },
+  
+//     {
+//       title: "Todo 5",
+//       desc: "This is my fifth Todo",
+//       completed: true,
+//     },
+//   ];
+
 const router = async function(req, res) {
 
     // GET: /api/todos
